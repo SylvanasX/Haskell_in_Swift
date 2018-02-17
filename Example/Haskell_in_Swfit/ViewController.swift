cadd81fa7e2ccc20f3f920ff734602dbc5449169
SylvanasX <10393625@qq.com>
2018-02-17 11:28:39 +0800

    curry

--- Example/Haskell_in_Swfit/ViewController.swift
+++ Example/Haskell_in_Swfit/ViewController.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import Haskell_in_Swfit
+
+struct Person {
+    var age: Int
+    var name: String
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        testCurry()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func testCurry() {
+        let p = curry(Person.init)(11)("mike")
+        print(p.age)
+        print(p.name)
+    }
 
 }
+
+
 
