f812a059b23781167b6177d6b81407b4a0ac09a7
SylvanasX <10393625@qq.com>
2018-03-07 14:33:00 +0800

    update

--- Haskell_in_Swfit/Classes/Function.swift
+++ Haskell_in_Swfit/Classes/Function.swift
@@ -5,4 +5,12 @@
 //  Created by Sylvanas on 07/03/2018.
 //
 
-import Foundation
+public func |> <A, B> (a: A, f: (A) -> B) -> B {
+    return f(a)
+}
+
+public func >>> <A, B, C> (f: @escaping (A) -> B, g: @escaping (B) -> C) -> ((A) -> C) {
+    return { a in
+        g(f(a))
+    }
+}
--- Haskell_in_Swfit/Classes/Operators.swift
+++ Haskell_in_Swfit/Classes/Operators.swift
@@ -5,4 +5,16 @@
 //  Created by Sylvanas on 07/03/2018.
 //
 
-import Foundation
+precedencegroup LeftApplyPrecedence {
+    associativity: left
+    higherThan: AssignmentPrecedence
+}
+
+precedencegroup FunctionCompositionPrecedence {
+    associativity: right
+    higherThan: LeftApplyPrecedence
+}
+
+infix operator |> : LeftApplyPrecedence
+
+infix operator >>> : FunctionCompositionPrecedence
